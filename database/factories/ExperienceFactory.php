c91e1ca957f8a5acb4260aa746ed9abaaa2305b5
Stefano Micciche <miccichestefano20@gmail.com>
2025-03-20 11:40:49 +0100

    feat: Experience Factory

---
+++ database/factories/ExperienceFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Experience>
+ */
+class ExperienceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+            return [
+                        'title' => $this->faker->name(),
+                        'category' => $this->faker->randomElement(['ceramica' , 'escritura' , 'acuarela' , 'cocina' , 'fotografia' , 'cosmetica']),
+                        'location_id' => Location::all()->random()->id,
+                        'price' => $this->faker->randomNumber(),
+                        'image' => $faker->imageUrl(360 , 360);
+                        'format' => $this->faker->randomElement(['presencial' , 'online' , 'hibrido']);
+                        'description' => $this->faker->text();
+                        'company' => $this->faker->company();
+                    ];
+    }
+}
