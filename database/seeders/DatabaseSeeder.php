23f3a06da991526c52ad82181fea7e79062e42e3
Stefano Micciche <miccichestefano20@gmail.com>
2025-03-20 13:08:23 +0100

    refactor: Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,9 @@ use App\Models\User;
 use App\Models\Cart;
 use App\Models\Location;
 use App\Models\Experience;
+use App\Models\Category;
+use App\Models\Company;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -23,6 +26,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
+        Category::factory(6)->create();
+        Company::factory(10)->create();
         Location::factory(10)->create();
         Experience::factory(10)->create();
         Cart::factory(10)->create();
